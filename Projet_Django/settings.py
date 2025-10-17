0b17148cfd592c5dd4417ba169fd1387e7ac2bba
YvesGuellec <yves.guellec@gmail.com>
2025-10-17 13:59:27 +0200

    Ajout whitenoise et les fichiers static dans le settings

--- Projet_Django/settings.py
+++ Projet_Django/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'Projet_Django.urls'
@@ -92,6 +93,8 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # --- Fichiers médias ---
 MEDIA_URL = '/media/'
